248ff9521859f1b122924598f9bfb4a31e86e7f3
James Evans <joesaysahoy@gmail.com>
2023-11-19 09:45:27 -0800

    first

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Chagrid',
+    version='0.1',
+    author='James Evans',
+    author_email='joesaysahoy@gmail.com',
+    packages=find_packages(),
+    install_requires=['CharPackage', 'CharCore', 'gridengine_framework'],
+    license='LICENSE.txt',
+    description='A package for creating and managing grids of characters.',
+    long_description=open('README.txt').read(),
+    url='https://github.com/primal-coder/Chagrid'
+)
